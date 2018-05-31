6174fa445aa666e3b2bf8dfb388bd63b2e238394
julien <juliencauwet@yahoo.fr>
2018-05-31 12:55:08 +0200

    création de BookService et Repositoryet première méthodes d'accès BD

--- src/main/java/com/openclassrooms/BibliobackApplication.java
+++ src/main/java/com/openclassrooms/BibliobackApplication.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.biblioback;
+package com.openclassrooms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/openclassrooms/com/openclassrooms/services/BookService.java
@@ -0,0 +1,26 @@
+package com.openclassrooms.com.openclassrooms.services;
+
+import com.openclassrooms.biblioback.Book;
+import com.openclassrooms.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public void addBook(Book book){
+        bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks(){
+        List<Book> books = new ArrayList<>();
+        bookRepository.findAll().forEach(books::add);
+        return books;
+    }
+}
