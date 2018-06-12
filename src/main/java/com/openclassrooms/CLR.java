1383cb17cb01b516ba854ee92bba75473d968a55
julien <juliencauwet@yahoo.fr>
2018-06-12 15:35:22 +0200

    ajout de données dans le CLR

--- src/main/java/com/openclassrooms/CLR.java
+++ src/main/java/com/openclassrooms/CLR.java
@@ -3,6 +3,7 @@ package com.openclassrooms;
 import com.openclassrooms.biblioback.ws.Book;
 import com.openclassrooms.entities.AppUser;
 import com.openclassrooms.entities.BookEntity;
+import com.openclassrooms.services.AppUserService;
 import com.openclassrooms.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +14,8 @@ public class CLR implements CommandLineRunner{
 
     @Autowired
     BookService bookService;
+    @Autowired
+    AppUserService appUserService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -25,9 +28,14 @@ public class CLR implements CommandLineRunner{
         bookService.addBook(b3);
 
         AppUser u1 = new AppUser("Julien", "Cauwet", "juliencauwet@yahoo.fr", "12345", true);
-        AppUser u2 = new AppUser("Juan", "Olivero", "juliencauwet@yahoo.fr", "12345", true);
-        AppUser u3 = new AppUser("Manu", "Favre", "juliencauwet@yahoo.fr", "12345", true);
-        AppUser u4 = new AppUser("Laëtitia", "Cauwet", "juliencauwet@yahoo.fr", "12345", true);
+        AppUser u2 = new AppUser("Juan", "Olivero", "jjolivero83@gmail.com", "abcde", false);
+        AppUser u3 = new AppUser("Manu", "Favre", "juliencauwet@yahoo.fr", "vwxyz", false);
+        AppUser u4 = new AppUser("Laëtitia", "Cauwet", "juliencauwet@yahoo.fr", "98765", false);
+
+        appUserService.addUser(u1);
+        appUserService.addUser(u2);
+        appUserService.addUser(u3);
+        appUserService.addUser(u4);
 
         System.out.println("Quelques données ont été entrées!");
     }
